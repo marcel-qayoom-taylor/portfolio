16675c240b3b32213643ff76e89665d5835e88b7
Marcel Qayoom-Taylor <48072798+marcel-qayoom-taylor@users.noreply.github.com>
2024-06-10 21:52:56 +1000

    button size fix

--- src/components/sections/jobs.js
+++ src/components/sections/jobs.js
@@ -80,17 +80,19 @@ const StyledTabButton = styled.button`
   font-size: var(--fz-xs);
   text-align: left;
   white-space: nowrap;
+  flex-grow: 1;
 
   @media (max-width: 768px) {
     padding: 0 15px 2px;
   }
   @media (max-width: 600px) {
     ${({ theme }) => theme.mixins.flexCenter};
-    min-width: 120px;
+    width: auto;
     padding: 0 15px;
     border-left: 0;
     border-bottom: 2px solid var(--lightest-navy);
     text-align: center;
+    flex-grow: 1;
   }
 
   &:hover,
