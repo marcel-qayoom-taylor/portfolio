db6209f3db58216b27491f79f0a8ad256498a1ea
Marcel Qayoom-Taylor <48072798+marcel-qayoom-taylor@users.noreply.github.com>
2024-11-05 15:29:27 +1100

    remove filter on images

--- src/components/sections/featured.js
+++ src/components/sections/featured.js
@@ -256,7 +256,6 @@ const StyledProject = styled.li`
     a {
       width: 100%;
       height: 100%;
-      background-color: var(--green);
       border-radius: var(--border-radius);
       vertical-align: middle;
 
@@ -283,7 +282,6 @@ const StyledProject = styled.li`
         bottom: 0;
         z-index: 3;
         transition: var(--transition);
-        background-color: var(--navy);
         mix-blend-mode: screen;
       }
     }
@@ -291,13 +289,11 @@ const StyledProject = styled.li`
     .img {
       border-radius: var(--border-radius);
       mix-blend-mode: multiply;
-      filter: grayscale(100%) contrast(1) brightness(90%);
 
       @media (max-width: 768px) {
         object-fit: cover;
         width: auto;
         height: 100%;
-        filter: grayscale(100%) contrast(1) brightness(50%);
       }
     }
   }
